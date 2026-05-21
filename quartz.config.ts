8b63216ae5252607fc09a646fe4b35305847d722
jiayuan <chen_jiayuan@outlook.com>
2026-05-21 18:18:19 +0800

    chore: update username to archjarren

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "chen8866.github.io",
+    baseUrl: "archjarren.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
